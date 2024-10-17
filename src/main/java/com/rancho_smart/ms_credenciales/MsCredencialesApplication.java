27e8d801b7acefd6a2e363e9c489a340ac83eaa2
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-17 03:33:35 -0500

    :sparkles: feat: Creacion de la Aplicacion Springboot.

---
+++ src/main/java/com/rancho_smart/ms_credenciales/MsCredencialesApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_credenciales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCredencialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCredencialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rancho_smart/ms_credenciales/MsCredencialesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_credenciales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCredencialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
